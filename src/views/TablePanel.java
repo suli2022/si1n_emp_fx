82cea278baf864adf1f4de66a79ed3896be5bb75
Nagy Janos <nagy@zold.lan>
2023-03-27 16:03:25 +0200

    A név mező már látszik

--- src/views/TablePanel.java
+++ src/views/TablePanel.java
@@ -1,11 +1,23 @@
 package views;
 
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import models.Employee;
 
 public class TablePanel extends TableView<Employee> {
-
+    TableColumn<Employee, String> name_col;
     public TablePanel() {
+        this.createColumns();  
+        this.addColumns();      
+    }
+    private void createColumns() {
+        this.name_col = new TableColumn<>("Név");
+        this.name_col.setCellValueFactory(new PropertyValueFactory<>("name") );
+        
+    }
+    private void addColumns() {
+        this.getColumns().add(this.name_col);
     }
     
 }
